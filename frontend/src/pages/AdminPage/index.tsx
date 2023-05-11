a37bc76bf9f48f05888e409823a7eb99af707f3c
ZayarTun71 <zayarjob@gmail.com>
2023-05-11 14:20:31 +0630

    dropdown

--- frontend/src/pages/AdminPage/index.tsx
+++ frontend/src/pages/AdminPage/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
+import Dropdown from "../../components/DropDown";
+
 const AdminDashboard = () => {
+  function onSubmitHandle(ev: React.FormEvent<HTMLFormElement>) {
+    ev.preventDefault();
+  }
+
+  function onClickHandle() {}
   return (
     <div className="user_form">
-      <form action="">
+      <form action="" onClick={onSubmitHandle}>
         <Input label="Name" errorMessage="*require" placeholder="Name..." />
         <Input label="Email" errorMessage="*require" placeholder="Email.." />
         <Input
@@ -17,7 +24,17 @@ const AdminDashboard = () => {
           errorMessage="*require"
           placeholder="Comfirm Password..."
         />
-        <Button type="button" label="Create" className="btn btn--user_create"/>
+        <Dropdown
+          placement="bottom"
+          buttonChildren={"Role"}
+          dropdownChildren={"Admin"}
+        />
+        <Button
+          type="button"
+          label="Create"
+          className="btn btn--user_create"
+          onClick={onClickHandle}
+        />
       </form>
     </div>
   );
