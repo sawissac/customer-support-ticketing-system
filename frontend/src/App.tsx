4272aeffe8a72945e52608ff9fac89614cc8ad7a
sawissac <issacmm64@gmail.com>
2023-05-11 10:59:41 +0630

    save current change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,13 +9,6 @@ function App() {
     <React.Fragment>
       <SideBar route="/admin-dashboard" />
       <Routes>
-        <Route
-          path="/"
-          loader={({ params }) => {
-            alert("hello"); 
-            return 'hello'
-          }}
-        />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
--- frontend/src/redux/feature_slice/AuthSlice.ts
+++ frontend/src/redux/feature_slice/AuthSlice.ts
@@ -1,6 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = "";
+const initialState:AuthInterface = {
+  token: "",
+  user: {
+    id: 0,
+    name: "",
+  },
+  auth: false,
+  role: ""
+};
 
 const authSlice = createSlice({
   name: "AuthSlice",
