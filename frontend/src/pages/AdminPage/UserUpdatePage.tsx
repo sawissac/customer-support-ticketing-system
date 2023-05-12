dff393292c775654023852f7f00ecfaf699e5aec
ZayarTun71 <zayarjob@gmail.com>
2023-05-12 20:33:01 +0630

    user update page

--- frontend/src/pages/AdminPage/UserUpdatePage.tsx
+++ frontend/src/pages/AdminPage/UserUpdatePage.tsx
@@ -1,35 +1,22 @@
 import React from "react";
 import Nav from "../../components/Nav";
-import { IconUserPlus } from "@tabler/icons-react";
+import { IconUserPlus, IconUserUp } from "@tabler/icons-react";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import Dropdown from "../../components/DropDown";
 import { IconMenuOrder } from "@tabler/icons-react";
-import TicketList from "../../components/TicketList";
-import UserUpdatePage from "./UserUpdatePage";
 
-const UserCreatePage = () => {
+const UserUpdatePage = () => {
   function onSubmitHandle(ev: React.FormEvent<HTMLFormElement>) {
     ev.preventDefault();
   }
   function onClickHandle() {}
   return (
     <div className="admin-container">
-      <Nav icon={<IconUserPlus />} label="User Create Page" />
-      <TicketList
-        description="I can,t pess my button"
-        name="Zayar"
-        company="Ace"
-        priority="critical"
-        status="open"
-        day="1 day ago"
-      />
-      <UserUpdatePage/>
+      <Nav icon={<IconUserUp />} label="User Update Page" />
       <form action="" onClick={onSubmitHandle} className="form-container">
         <Input label="Name" errorMessage="*require" placeholder="Name..." />
         <Input label="Email" errorMessage="*require" placeholder="Email.." />
-        <Input label="Password" errorMessage="*require" placeholder="Password..." />
-        <Input label="Comfirm Password" errorMessage="*require" placeholder="Comfirm Password..." />
         <Dropdown
           placement="bottom"
           buttonClassName="form-dropdown-btn"
@@ -46,10 +33,10 @@ const UserCreatePage = () => {
             </>
           }
         />
-        <Button type="button" label="Create" className="btn btn--user_create" onClick={onClickHandle} />
+        <Button type="button" label="Update" className="btn btn--user-update" onClick={onClickHandle} />
       </form>
     </div>
   );
 };
 
-export default UserCreatePage;
+export default UserUpdatePage;
