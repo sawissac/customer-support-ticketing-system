f2bfe67a8caaaf0689673148c173971dc2d85fef
ZayarTun71 <zayarjob@gmail.com>
2023-05-12 12:47:18 +0630

    merged

---
+++ frontend/src/components/TicketList/index.tsx
@@ -0,0 +1,16 @@
+import { IconBuildingSkyscraper, IconClockHour3, IconFlag3Filled, IconStarsFilled, IconUser } from '@tabler/icons-react'
+import React from 'react'
+
+ const TicketList = () => {
+  return (
+    <div>
+        <IconBuildingSkyscraper/>
+        <IconStarsFilled/>
+        <IconFlag3Filled/>
+        <IconUser/>
+        <IconClockHour3/>
+    </div>
+    
+  )
+}
+export default TicketList
