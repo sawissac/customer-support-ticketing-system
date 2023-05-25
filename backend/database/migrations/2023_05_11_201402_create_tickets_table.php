920856a2d572eec575bccaeecde456825d25890b
ZayYarNiang <zayarnaing.pp@gmail.com>
2023-05-25 16:12:50 +0630

    added

--- backend/database/migrations/2023_05_11_201402_create_tickets_table.php
+++ backend/database/migrations/2023_05_11_201402_create_tickets_table.php
@@ -27,7 +27,6 @@ return new class extends Migration
             $table->date('start_date')->nullable();
             $table->date('end_date')->nullable();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
