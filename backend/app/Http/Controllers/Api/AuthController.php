86b3a30dd1561b1a878dd3468dd81cfef34abff0
Htet Wai Phyo Nai <youngartic.ru@gmail.com>
2023-05-29 15:06:03 +0630

    hwpn

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -27,7 +27,6 @@ class AuthController extends Controller
                     'errors' => $validateUser->errors()
                 ], 401);
             }
-
             if(!Auth::attempt($request->only(['email', 'password']))){
                 return response()->json([
                     'status' => false,
