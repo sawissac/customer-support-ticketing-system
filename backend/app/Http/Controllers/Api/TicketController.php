61b4344aaa6c363d79f27b31910c5c33b7fe5936
ZayarTun71 <zayarjob@gmail.com>
2023-05-31 16:36:26 +0630

    add

--- backend/app/Http/Controllers/Api/TicketController.php
+++ backend/app/Http/Controllers/Api/TicketController.php
@@ -51,7 +51,7 @@ class TicketController extends BaseController
                 'description' => 'required',
                 'status' => 'nullable',
                 'priority' => 'nullable',
-                'drive_link' => 'nullable|url',
+                'drive_link' => 'nullable',
                 'admin_id' => 'nullabe',
                 'start_date' => 'nullable',
                 'end_date' => 'nullable',
@@ -63,7 +63,6 @@ class TicketController extends BaseController
         }
 
         $data = $this->ticketService->store($validate);
-
         return $this->sendResponse($data, 'Ticket created successfully.', 201);
     }
 
@@ -118,7 +117,7 @@ class TicketController extends BaseController
     {
         $result = $this->ticketRepo->checkTicketList();
 
-        return $this->sendResponse($result,'Monthly Tickets successfully.');
+        return $this->sendResponse($result, 'Monthly Tickets successfully.');
     }
 
     public function checkDate($id)
